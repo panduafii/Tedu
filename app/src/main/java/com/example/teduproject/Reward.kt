32aaa3ed376c22c212bab174e3385703aa000e2c
Fathriza <fathriza563@gmail.com>
2024-11-21 17:14:03 +0700

    push alfath

--- app/src/main/java/com/example/teduproject/Reward.kt
+++ app/src/main/java/com/example/teduproject/Reward.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Reward : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
