3d9868e711bc24b6351a8514bfcdfceb7d51186f
daffaramadhan <22523024@students.uii.ac.id>
2024-12-09 11:01:43 +0700

    push daffa

--- app/src/main/java/com/example/teduproject/MainPage.kt
+++ app/src/main/java/com/example/teduproject/MainPage.kt
@@ -10,7 +10,7 @@ class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_page)
+        setContentView(R.layout.activity_main_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
