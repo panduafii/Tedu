8265629d8f1f2d46c89e3a9c4388b17f8780eca2
pandu <pandulombok@gmail.com>
2024-12-16 19:38:58 +0700

    menambahkan fungsi untuk memanggil class modular agar bisa memanggil total poin atau xp

--- app/src/main/java/com/example/teduproject/MainActivity.kt
+++ app/src/main/java/com/example/teduproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.teduproject
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
 
         val navigationView = findViewById<View>(R.id.navigationCard)
         BottomNavigationHelper.setupBottomNavigation(this, navigationView)
+
+        val textTotalPoin = findViewById<TextView>(R.id.textTotalPoin)
+        FirebaseHelper.fetchTotalPoin(textTotalPoin, this)
+
     }
 }
